c153c7d5070ae5c1fb35ef9e427967e05da00041
Loan Courchinoux-Billonnet <74971600+Patais@users.noreply.github.com>
2024-05-14 22:58:35 +0200

    feat: create basic snake

--- main.py
+++ main.py
@@ -8,10 +8,27 @@ DEFAULT_BODY_SIZE = 2
 SNAKE_COLOR = '#FFFFFF'
 
 
-root = tk.Tk()
-root.title("Snake")
-root.resizable(False, False)
+class Snake:
+    def __init__(self):
+        self.body_size = DEFAULT_BODY_SIZE
+        self.coordinates = []
+        self.squares = []
 
-canvas = tk.Canvas(root, width=WIDTH * CELL_SIZE, height=HEIGHT * CELL_SIZE, bg="black")
+        for i in range(0, self.body_size):
+            self.coordinates.append([i + 2, 4])
+
+        for x, y in self.coordinates:
+            square = canvas.create_rectangle(x * CELL_SIZE, y * CELL_SIZE, x * CELL_SIZE + CELL_SIZE, y * CELL_SIZE + CELL_SIZE, fill=SNAKE_COLOR)
+            self.squares.append(square)
+
+
+window = tk.Tk()
+window.title("Snake")
+window.resizable(False, False)
+
+canvas = tk.Canvas(window, width=WIDTH * CELL_SIZE, height=HEIGHT * CELL_SIZE, bg="black")
 canvas.pack()
-root.mainloop()
+
+snake = Snake()
+
+window.mainloop()
